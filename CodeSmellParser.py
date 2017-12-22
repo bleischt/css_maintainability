8afdf19fa43ff6f4208971cd2be2563156825b84
Tobias Bleisch <bleischt@hotmail.com>
2017-12-22 01:08:50 -0800

    modified parser to take options to exclude prolematic code smells

--- CodeSmellParser.py
+++ CodeSmellParser.py
@@ -8,14 +8,21 @@ def checkArgs():
         exit()
     return sys.argv[1]
 
-def readCillaFile(filepath):
-    with open('{}/cilla.txt'.format(filepath), 'r') as f:
-       text = f.read()
-       if 'chrome-error://chromewebdata/' in text or not text:
-           return None
-       text = text.replace(' from which:','').replace('(:link, :hover, etc)', '')
-       lines = text.split('\n')
-       lines = lines[:lines.index('PERCENTAGE: ') + 1]
+def readCillaFile(filepath, removeChromeError=True, removeConnectionRefused=True):
+    try:
+        with open('{}/cilla.txt'.format(filepath), 'r') as f:
+           if removeConnectionRefused:
+              with open('{}/cilla.log'.format(filepath), 'r') as ff:
+                 if 'Connection refused' in ff.read():
+                    return None
+           text = f.read()
+           if not text or removeChromeError and ('chrome-error://chromewebdata/' in text or 'data:' in text):
+               return None
+           text = text.replace(' from which:','').replace('(:link, :hover, etc)', '')
+           lines = text.split('\n')
+           lines = lines[:lines.index('PERCENTAGE: ') + 1]
+    except IOError as e:
+        lines = None
     return lines
 
 def extractCodeSmellsFromText(lines):
@@ -25,11 +32,11 @@ def extractCodeSmellsFromText(lines):
         return smells
     return None
 
-def getCodeSmellsPerSite(sitesDir, sitesList):
+def getCodeSmellsPerSite(sitesDir, sitesList, removeChromeError=True, removeConnectionRefused=True):
     smells = dict()
     for site in sitesList:
         try:
-            lines = readCillaFile(sitesDir + '/' + site)
+            lines = readCillaFile(sitesDir + '/' + site, removeChromeError, removeConnectionRefused)
         except IOError:
             print('Error reading file: {}/{}/cilla.txt'.format(sitesDir, site))
             print('moving on...')
@@ -44,11 +51,11 @@ def discardInvalidSmellSets(sitesToSmells):
             toSave[site] = smells
     return toSave
 
-def readCodeSmells(sitesDir):
+def readCodeSmells(sitesDir, removeChromeError=True, removeConnectionRefused=True):
     #sitesDir = checkArgs()
     sites = [name for name in os.listdir(sitesDir) if os.path.isdir('{}/{}'.format(sitesDir, name))]
     sites = [site for site in sites if 'cilla.txt' in os.listdir('{}/{}'.format(sitesDir, site))]
-    sitesToSmells = getCodeSmellsPerSite(sitesDir, sites)
+    sitesToSmells = getCodeSmellsPerSite(sitesDir, sites, removeChromeError, removeConnectionRefused)
     sitesToSmells = discardInvalidSmellSets(sitesToSmells)
     return sitesToSmells 
 
